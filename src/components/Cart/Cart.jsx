2fb2f1ea7a64c48623b6d6511482dc9cd7481c19
unknown <M.alamiidrissi@aui.ma>
2021-03-08 23:44:58 +0100

    added stripe settings

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,53 +1,49 @@
-import React from 'react'
+import React from 'react';
 import { Container, Typography, Button, Grid } from '@material-ui/core';
-import useStyles from './styles'
-import CartItem from './cartItem/CartItem'
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
+import CartItem from './cartItem/CartItem';
+import useStyles from './styles';
 
-const Cart = ({ cart, handleUpdateCartQty, handleRemoveFromcart, handleEmptyCart }) => {
-
+const Cart = ({ cart, onUpdateCartQty, onRemoveFromCart, onEmptyCart }) => {
     const classes = useStyles();
-    const Emptycard = () => (
-        <Typography variant="subtitle1"> you have no items in your shopping cart
-            <Link to='/' className={classes.link}> Start adding some!</Link></Typography>
 
+    const handleEmptyCart = () => onEmptyCart();
+
+    const renderEmptyCart = () => (
+        <Typography variant="subtitle1">You have no items in your shopping cart,
+            <Link className={classes.link} to="/">start adding some</Link>!
+        </Typography>
     );
-    const FilledCart = () => (
+
+    if (!cart.line_items) return 'Loading';
+
+    const renderCart = () => (
         <>
             <Grid container spacing={3}>
-                {cart.line_items.map((item) => (
-                    <Grid item xs={12} sm={4} key={item.id}>
-                        <CartItem item={item} onUpdateCartQty={handleUpdateCartQty} onRemoveFromcart={handleRemoveFromcart} />
-
+                {cart.line_items.map((lineItem) => (
+                    <Grid item xs={12} sm={4} key={lineItem.id}>
+                        <CartItem item={lineItem} onUpdateCartQty={onUpdateCartQty} onRemoveFromCart={onRemoveFromCart} />
                     </Grid>
-
                 ))}
-
             </Grid>
-            <div className={classes.CardDetails}>
-                <Typography variant="h4">
-                    Subtotal: {cart.subtotal.formatted_with_symbol}
-                </Typography>
+            <div className={classes.cardDetails}>
+                <Typography variant="h4">Subtotal: {cart.subtotal.formatted_with_symbol}</Typography>
                 <div>
-                    <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary" onClick={handleEmptyCart} >Empty cart</Button>
-                    <Button component={Link} to='/checkout' className={classes.checkoutButton} size="large" type="button" variant="contained" color="primary">Checkout</Button>
-
+                    <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary" onClick={handleEmptyCart}>Empty cart</Button>
+                    <Button className={classes.checkoutButton} component={Link} to="/checkout" size="large" type="button" variant="contained" color="primary">Checkout</Button>
                 </div>
-
             </div>
         </>
-    )
-    if (!cart.line_items) return 'loading...';
+    );
 
     return (
         <Container>
             <div className={classes.toolbar} />
             <Typography className={classes.title} variant="h3" gutterBottom>Your Shopping Cart</Typography>
-            {!cart.line_items.length ? <Emptycard /> : <FilledCart />}
-
+            { !cart.line_items.length ? renderEmptyCart() : renderCart()}
         </Container>
-    )
-}
+    );
+};
 
 export default Cart;
